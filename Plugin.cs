65da53d1d2a70f1e9cfeb3b12ab50388eba3c316
Jacob1Tm <84023677+Jacob1Tm@users.noreply.github.com>
2025-07-05 17:32:54 -0700

    remove weird space

--- Plugin.cs
+++ Plugin.cs
@@ -27,70 +27,70 @@ namespace JacobsToolbox
             Instance = this;
             base.OnEnabled();
             CustomHandlersManager.RegisterEventsHandler(Events);
-             PlayerHandler.Verified += EventHandlers.General.OnVerified;
-             PlayerHandler.Died += EventHandlers.General.OnDeath;
-             PlayerHandler.Left += EventHandlers.General.OnLeft;
-             PlayerHandler.ChangingRole += EventHandlers.General.OnChangingRole;
-             Scp173Handler.BeingObserved += EventHandlers.KomarEvents.OnBeingObserved;
-             Scp096Handler.AddingTarget += EventHandlers.KomarEvents.OnAddingTarget;
-             Scp939Handler.ValidatingVisibility += EventHandlers.KomarEvents.OnValidatingVisibility;
-             Scp939Handler.PlayingFootstep += EventHandlers.KomarEvents.OnPlayingFootstep;
-             PlayerHandler.RemovingHandcuffs += EventHandlers.KomarEvents.OnRemovingHandcuffs;
-             Scp939Handler.SavingVoice += EventHandlers.KomarEvents.OnSavingVoice;
-             PlayerHandler.TriggeringTesla += EventHandlers.KomarEvents.OnTriggeringTesla;
-             PlayerHandler.IntercomSpeaking += EventHandlers.KomarEvents.OnIntercomSpeaking; 
-             ServerHandler.RespawningTeam += EventHandlers.KomarEvents.OnRespawningTeam;
-             Scp914Handler.UpgradingPickup += EventHandlers.General.OnUpgradingPickup;
-             Scp914Handler.UpgradingInventoryItem += EventHandlers.General.OnUpgradingInventoryItem;
-             ServerSpecificSettingsSync.ServerOnSettingValueReceived += EventHandlers.CustomKeybinds.KeybindExample;
-             PlayerHandler.FlippingCoin += EventHandlers.General.OnFlippingCoin;
-             PlayerHandler.Hurting += EventHandlers.General.OnHurting;
+            PlayerHandler.Verified += EventHandlers.General.OnVerified;
+            PlayerHandler.Died += EventHandlers.General.OnDeath;
+            PlayerHandler.Left += EventHandlers.General.OnLeft;
+            PlayerHandler.ChangingRole += EventHandlers.General.OnChangingRole;
+            Scp173Handler.BeingObserved += EventHandlers.KomarEvents.OnBeingObserved;
+            Scp096Handler.AddingTarget += EventHandlers.KomarEvents.OnAddingTarget;
+            Scp939Handler.ValidatingVisibility += EventHandlers.KomarEvents.OnValidatingVisibility;
+            Scp939Handler.PlayingFootstep += EventHandlers.KomarEvents.OnPlayingFootstep;
+            PlayerHandler.RemovingHandcuffs += EventHandlers.KomarEvents.OnRemovingHandcuffs;
+            Scp939Handler.SavingVoice += EventHandlers.KomarEvents.OnSavingVoice;
+            PlayerHandler.TriggeringTesla += EventHandlers.KomarEvents.OnTriggeringTesla;
+            PlayerHandler.IntercomSpeaking += EventHandlers.KomarEvents.OnIntercomSpeaking; 
+            ServerHandler.RespawningTeam += EventHandlers.KomarEvents.OnRespawningTeam;
+            Scp914Handler.UpgradingPickup += EventHandlers.General.OnUpgradingPickup;
+            Scp914Handler.UpgradingInventoryItem += EventHandlers.General.OnUpgradingInventoryItem;
+            ServerSpecificSettingsSync.ServerOnSettingValueReceived += EventHandlers.CustomKeybinds.KeybindExample;
+            PlayerHandler.FlippingCoin += EventHandlers.General.OnFlippingCoin;
+            PlayerHandler.Hurting += EventHandlers.General.OnHurting;
 
-             _settings =
-             [
-                 new HeaderSetting("Jacob's Toolbox", "A collection of various features and enhancements for SCP:SL."),
-                 new KeybindSetting(24, "Podnoszenie monet jako SCP", KeyCode.Z, hintDescription: "Pozwala graczowi na podniesienie monmety jako SCP po celowaniu na nią."),
-             ];
-             try
-             {
-                 _harmony = new Harmony(nameof(JacobsToolbox).ToLowerInvariant() + "-" + DateTime.UtcNow.Ticks);
-                 _harmony.PatchAll();
+            _settings =
+            [
+                new HeaderSetting("Jacob's Toolbox", "A collection of various features and enhancements for SCP:SL."),
+                new KeybindSetting(24, "Podnoszenie monet jako SCP", KeyCode.Z, hintDescription: "Pozwala graczowi na podniesienie monmety jako SCP po celowaniu na nią."),
+            ];
+            try
+            {
+                _harmony = new Harmony(nameof(JacobsToolbox).ToLowerInvariant() + "-" + DateTime.UtcNow.Ticks);
+                _harmony.PatchAll();
 
-                 Log.Info("Harmony patching complete.");
-             }
-             catch (Exception e)
-             {
-                 Log.Error($"Harmony patching failed! {e}");
-             }
+                Log.Info("Harmony patching complete.");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Harmony patching failed! {e}");
+            }
 
-             SettingBase.Register(_settings);
+            SettingBase.Register(_settings);
         }
 
         public override void OnDisabled()
         {
             Instance = null;
             CustomHandlersManager.UnregisterEventsHandler(Events);
-             PlayerHandler.Verified -= EventHandlers.General.OnVerified;
-             PlayerHandler.Died -= EventHandlers.General.OnDeath;
-             PlayerHandler.Left -= EventHandlers.General.OnLeft;
-             PlayerHandler.ChangingRole -= EventHandlers.General.OnChangingRole;
-             Scp173Handler.BeingObserved -= EventHandlers.KomarEvents.OnBeingObserved;
-             Scp096Handler.AddingTarget -= EventHandlers.KomarEvents.OnAddingTarget;
-             Scp939Handler.ValidatingVisibility -= EventHandlers.KomarEvents.OnValidatingVisibility;
-             Scp939Handler.PlayingFootstep -= EventHandlers.KomarEvents.OnPlayingFootstep;
-             PlayerHandler.RemovingHandcuffs -= EventHandlers.KomarEvents.OnRemovingHandcuffs;
-             Scp939Handler.SavingVoice -= EventHandlers.KomarEvents.OnSavingVoice;
-             PlayerHandler.TriggeringTesla -= EventHandlers.KomarEvents.OnTriggeringTesla;
-             PlayerHandler.IntercomSpeaking -= EventHandlers.KomarEvents.OnIntercomSpeaking;
-             ServerHandler.RespawningTeam -= EventHandlers.KomarEvents.OnRespawningTeam;
-             Scp914Handler.UpgradingPickup -= EventHandlers.General.OnUpgradingPickup;
-             Scp914Handler.UpgradingInventoryItem -= EventHandlers.General.OnUpgradingInventoryItem;
-             ServerSpecificSettingsSync.ServerOnSettingValueReceived -= EventHandlers.CustomKeybinds.KeybindExample;
-             PlayerHandler.FlippingCoin -= EventHandlers.General.OnFlippingCoin;
-             PlayerHandler.Hurting -= EventHandlers.General.OnHurting;
-             
-             _harmony.UnpatchAll();
-             _harmony = null;
+            PlayerHandler.Verified -= EventHandlers.General.OnVerified;
+            PlayerHandler.Died -= EventHandlers.General.OnDeath;
+            PlayerHandler.Left -= EventHandlers.General.OnLeft;
+            PlayerHandler.ChangingRole -= EventHandlers.General.OnChangingRole;
+            Scp173Handler.BeingObserved -= EventHandlers.KomarEvents.OnBeingObserved;
+            Scp096Handler.AddingTarget -= EventHandlers.KomarEvents.OnAddingTarget;
+            Scp939Handler.ValidatingVisibility -= EventHandlers.KomarEvents.OnValidatingVisibility;
+            Scp939Handler.PlayingFootstep -= EventHandlers.KomarEvents.OnPlayingFootstep;
+            PlayerHandler.RemovingHandcuffs -= EventHandlers.KomarEvents.OnRemovingHandcuffs;
+            Scp939Handler.SavingVoice -= EventHandlers.KomarEvents.OnSavingVoice;
+            PlayerHandler.TriggeringTesla -= EventHandlers.KomarEvents.OnTriggeringTesla;
+            PlayerHandler.IntercomSpeaking -= EventHandlers.KomarEvents.OnIntercomSpeaking;
+            ServerHandler.RespawningTeam -= EventHandlers.KomarEvents.OnRespawningTeam;
+            Scp914Handler.UpgradingPickup -= EventHandlers.General.OnUpgradingPickup;
+            Scp914Handler.UpgradingInventoryItem -= EventHandlers.General.OnUpgradingInventoryItem;
+            ServerSpecificSettingsSync.ServerOnSettingValueReceived -= EventHandlers.CustomKeybinds.KeybindExample;
+            PlayerHandler.FlippingCoin -= EventHandlers.General.OnFlippingCoin;
+            PlayerHandler.Hurting -= EventHandlers.General.OnHurting;
+            
+            _harmony.UnpatchAll();
+            _harmony = null;
             base.OnDisabled();
         }
     }
